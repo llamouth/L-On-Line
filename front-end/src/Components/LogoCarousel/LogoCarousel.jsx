69cf26d2256cdefb194ba10d8a9c9ca50b1b05f1
llamouth <150949923+llamouth@users.noreply.github.com>
2024-08-04 10:20:46 -0400

    fix: fix bug with photos on netlify

--- front-end/src/Components/LogoCarousel/LogoCarousel.jsx
+++ front-end/src/Components/LogoCarousel/LogoCarousel.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import "./LogoCarousel.scss"
+import image1 from "../../../Images/Lonline.png"
+import image2 from "../../../Images/Lonline2.png"
+import image3 from "../../../Images/Lonline3.png"
 
 
 const LogoCarousel = () => {
     return (
         <Carousel>
             <Carousel.Item>
-                <img src="../../Images/Lonline.png" className="d-block w-100" />
+                <img src={image1} className="d-block w-100" />
             </Carousel.Item>
             <Carousel.Item>
-                <img src="../../Images/Lonline2.png" className="d-block w-100" />
+                <img src={image2} className="d-block w-100" />
             </Carousel.Item>
             <Carousel.Item>                
-                <img src="../../Images/Lonline3.png" className="d-block w-100" />
+                <img src={image3} className="d-block w-100" />
             </Carousel.Item>
         </Carousel>
     );
